ebe2a7a43f9181b63b811085083931eeabb318ca
Humberto Machado <betolinck@gmail.com>
2016-04-04 21:42:36 -0300

    Usando libs extendas

--- Main.ts
+++ Main.ts
@@ -1,3 +1,14 @@
 ///<reference path="MyUser.ts"/>
+//declare var $: any;
+///<reference path="typings/browser.d.ts"/>
+
 var user = new MyUser('Devlogger');
 document.getElementById("greet").innerHTML = user.greet();
+
+//Usando Jquery
+$(document).ready(function(){
+    $('#greet').hide();
+    $( 'button' ).on( 'click', function( event ) {
+        $('#greet').show();
+    });
+});
